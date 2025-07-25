25305e0e17699c3ad645c53d6558f97d3e389cb7
poohzo <684259030@wbemail.npru.ac.th>
2025-07-25 11:56:39 +0700

    first commit

---
+++ VerahlesExample.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class VerahlesExample {
+  public static void main(String args[]) {
+     char clothingSize='s';
+     int websiteVisitors = 0;
+     char studentGrade = 'A';
+     double studentGPA = 4.0;
+     final double PI = 3.14159;
+     float annualExpenses = 15000.00f;
+     final int MoNTHS_IN_YEAR =12;
+     System.out.println("Clothing Size"+clothingSize);
+     System.out.println("Website Visiors"+websiteVisitors);
+     System.out.println("Student Grad"+studentGrade);
+     System.out.println("Student GPA"+studentGPA);
+     System.out.println("Value of PI"+PI);
+     System.out.println("Annual Expenses"+annualExpenses);
+     System.out.println("MoNTHS_IN_YEAR"+MoNTHS_IN_YEAR);
+  }  
+}
