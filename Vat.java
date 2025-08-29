b62235c84b10904ab639f84df87b6a8ebdd86e99
poohzo <684259030@wbemail.npru.ac.th>
2025-08-29 11:48:43 +0700

    first commit;

---
+++ Vat.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Vat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Productname= ");
+        String N1 = scanner.nextLine();
+        System.out.print("Enter Productprice=");
+        double P1 = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Enter Productname =");
+        String N2 = scanner.nextLine();
+        System.out.print("Enter Productprice=");
+        double P2 = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Enter Productname =");
+        String N3 = scanner.nextLine();
+        System.out.print("Enter Productprice=");
+        double P3 = scanner.nextDouble();
+        System.out.print("Enter money=");
+        double money = scanner.nextDouble();
+        double sum = P1 + P2 + P3;
+        double vat = sum * 0.07;
+        double Net = sum + vat;
+        double Change = money - Net;
+        System.out.println("sum = " + sum);
+        System.out.println("vat =" + vat);
+        System.out.println("Net =" + Net);
+        System.out.println("Change =" + Change);
+    }
+}
